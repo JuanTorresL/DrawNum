20fb4dda0785764cdb354113e5f5bf37b6771b09
Carlos R. Villanueva Ousset <crvo84@gmail.com>
2017-04-14 15:20:38 -0500

    Implement DrawView.

--- DrawNum/DrawView.swift
+++ DrawNum/DrawView.swift
@@ -2,11 +2,12 @@
 //  DrawView.swift
 //  DrawNum
 //
-//  Created by Carlos Rogelio Villanueva Ousset on 4/14/17.
+//  Created by Juan Torres & Carlos Villanueva on 4/14/17.
 //  Copyright © 2017 SistemasInteligentes-ITESM-JCTorres-CVillanueva. All rights reserved.
 //
 
 import UIKit
+import SnapKit
 
 class DrawView: UIView {
     
@@ -18,11 +19,125 @@ class DrawView: UIView {
         static let brush = UIColor.blue
     }
     
-    @IBOutlet weak var mainImageView: UIImageView!
-    @IBOutlet weak var tempImageView: UIImageView!
+    private var mainImageView = UIImageView()
+    private var tempImageView = UIImageView()
     
     private var lastPoint = CGPoint.zero
     private var swiped = false
+    
+    
+    /* Initialization */
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
 
+        initialSetup()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        initialSetup()
+    }
+    
+    private func initialSetup() {
+        addSubview(mainImageView)
+        mainImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        addSubview(tempImageView)
+        tempImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+
+    
+    /* Drawing */
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first {
+            lastPoint = touch.location(in: self)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: self)
+            drawLine(from: lastPoint, to: currentPoint)
+            
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            // draw single point
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        
+        // Merge tempImageView into mainImageView
+        UIGraphicsBeginImageContext(mainImageView.frame.size)
+        mainImageView.image?.draw(in: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        mainImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        tempImageView.image = nil
+    }
+    
+    private func drawLine(from: CGPoint, to: CGPoint) {
+        // 1
+        UIGraphicsBeginImageContext(frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        tempImageView.image?.draw(in: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+        
+        // 2
+        context?.move(to: from)
+        context?.addLine(to: to)
+
+        // 3
+        context?.setLineCap(.round)
+        context?.setLineWidth(Geometry.brushWidth)
+        context?.setStrokeColor(Color.brush.cgColor)
+        context?.setBlendMode(.normal)
+
+        // 4
+        context?.strokePath()
+
+        // 5
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
